7dead26119c78c304170905ae52dfdd5b0a25fb3
neeluri <neeluru.mani@gmail.com>
2026-02-14 14:30:20 +0530

    Modified Files

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,66 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        Product p = new Product();
+        p.setName("Pizza");
+        p.setCategory("Food");
+        p.setQuantity(1);
+        p.setDescription("Cheese Pizza");
+        p.setActive(true);
+        p.setPrice(10000);
+        p.setSku("p1");
+
+        session.save(p);
+
+        t.commit();
+        session.close();
+
+        System.out.println("Product Created");
+
+        session = factory.openSession();
+
+        Product readProduct = session.get(Product.class, p.getId());
+        System.out.println("Product Name: " + readProduct.getName());
+
+        session.close();
+
+        session = factory.openSession();
+        t = session.beginTransaction();
+
+        Product updateProduct = session.get(Product.class, p.getId());
+        updateProduct.setPrice(12000);
+
+        t.commit();
+        session.close();
+
+        System.out.println("Product Updated");
+
+        session = factory.openSession();
+        t = session.beginTransaction();
+
+        Product deleteProduct = session.get(Product.class, p.getId());
+        session.delete(deleteProduct);
+
+        t.commit();
+        session.close();
+
+        System.out.println("Product Deleted");
+
+        factory.close();
     }
 }
---
+++ src/main/java/com/example/Product.java
@@ -0,0 +1,92 @@
+package com.example;
+
+import javax.persistence.*;
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private String category;
+    @Column
+    private int quantity;
+    @Column
+    private double price;
+    @Column(nullable = false, unique = true)
+    private String sku;
+    @Column
+    private boolean active;
+
+    public Product() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
+}
